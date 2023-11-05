d9b5bbe99c6709afdd299952d1a9b52a8a2575c5
Priyadarshi Kunal <priyadarshikunal2004@gmail.com>
2023-11-05 14:11:31 +0530

    Updates the outcome page to show outcome from the backend

--- src/components/pages/outcome.tsx
+++ src/components/pages/outcome.tsx
@@ -4,8 +4,14 @@ import {useEffect, useState} from "react";
 import {useParams} from 'react-router-dom';
 import {SERVICE_ENDPOINT} from "../../utils";
 
+interface IOutcome {
+    requestedLoanAmount?:Number,
+    approvedLoanAmount?:Number,
+    preAssessment?: Number
+}
+
 const Outcome = ()=>{
-    const [outcome, setOutcome] = useState('');
+    const [outcome, setOutcome] = useState<IOutcome>(null);
     const {applicationId} = useParams();
 
     useEffect(()=>{
@@ -18,7 +24,15 @@ const Outcome = ()=>{
     return <>
         <Header/>
         <Main progress={100}>
-            <h1>{outcome}</h1>
+            <h1>Business Loan Request Outcome</h1>
+            {outcome &&
+                <>
+                    <h2>Requested Loan Amount: {outcome.requestedLoanAmount!}</h2>
+                    <h2>Approved Loan Amount: {outcome.approvedLoanAmount!}</h2>
+                    <p>Your approved amount reflects your business's performance during last twelve months.</p>
+                    <p>Calculated at ${outcome.preAssessment}% of requested amount.</p>
+                </>
+            }
         </Main>
     </>
 };
